586ef45d1a233e0324e68a6164e8ab27f520771d
PancakeTAS <pancake@mgnet.work>
2024-03-23 19:58:57 +0100

    fix: properly shutdown bot

--- src/findseed.c
+++ src/findseed.c
@@ -76,9 +76,6 @@
 /// Addend for the seed
 #define ADDEND 0xBULL
 
-/// Discord client instance
-static struct discord *discord_client = NULL;
-
 /**
  * Generate the next float from the seed (Java Random)
  *
@@ -133,35 +130,35 @@ static char* corrupt_text(char* text, char* corrupted) {
  */
 static void handle_sigint(int signum) {
     log_info("[FINDSEED] Received SIGINT, shutting down bot...");
-    if (discord_client) discord_shutdown(discord_client);
+    ccord_shutdown_async();
 }
 
 /**
  * Initialize discord client
  *
- * \return 0 on success, 1 on failure
+ * \return Discord client on success, NULL on failure
  */
-static int initialize_discord() {
+static struct discord* initialize_discord() {
     // initialize concord
     CCORDcode code = ccord_global_init();
     if (code) {
         log_trace("[FINDSEED] ccord_global_init() failed: %d", code);
 
-        return 1;
+        return NULL;
     }
     log_trace("[FINDSEED] ccord_global_init() success");
 
     // create discord client
-    discord_client = discord_config_init(CONFIG_FILE);
-    if (!discord_client) {
+    struct discord* client = discord_config_init(CONFIG_FILE);
+    if (!client) {
         log_trace("[FINDSEED] discord_create() failed");
 
         ccord_global_cleanup();
-        return 1;
+        return NULL;
     }
     log_trace("[FINDSEED] discord_create() success");
 
-    return 0;
+    return client;
 }
 
 /**
@@ -372,7 +369,8 @@ void bot_main(struct discord *client, const struct discord_ready *event) {
 int main() {
     // initialize discord bot
     log_info("[FINDSEED] Initializing findseed discord bot...");
-    if (initialize_discord()) {
+    struct discord* client = initialize_discord();
+    if (!client) {
         log_fatal("[FINDSEED] Failed to initialize discord bot");
 
         return EXIT_FAILURE;
@@ -381,12 +379,12 @@ int main() {
     // run discord bot
     log_info("[FINDSEED] Launching findseed discord bot...");
     signal(SIGINT, handle_sigint);
-    discord_set_on_ready(discord_client, bot_main);
-    CCORDcode code = discord_run(discord_client);
+    discord_set_on_ready(client, bot_main);
+    CCORDcode code = discord_run(client);
 
     // cleanup discord bot
     log_info("[FINDSEED] Discord bot exited (%d), cleaning up...", code);
-    discord_cleanup(discord_client);
+    discord_cleanup(client);
     ccord_global_cleanup();
     return EXIT_SUCCESS;
 }
